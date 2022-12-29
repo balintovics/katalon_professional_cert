c4548dad0ab55207eb67603f1a8574a1379c5781
balintovics <molnar99b@gmail.com>
2022-12-29 15:14:40 +0100

    [feat] non-rapidapi rawg endpoints added

---
+++ Object Repository/RAWG/GET achievement details.rs
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GET achievement details</name>
+   <tag></tag>
+   <elementGuidId>ae03b522-41a3-4aa7-b7a1-35d3a01f5615</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>${GlobalVariable.hostHeader}</name>
+      <type>Main</type>
+      <value>${GlobalVariable.host}</value>
+      <webElementGuid>21245dfe-da27-43fa-8453-af3eceb391bf</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>${GlobalVariable.apiKeyHeader}</name>
+      <type>Main</type>
+      <value>${GlobalVariable.apiKey}</value>
+      <webElementGuid>f9a6fd8c-7d3f-4ebf-9820-6695dadc42df</webElementGuid>
+   </httpHeaderProperties>
+   <katalonVersion>8.5.5</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${GlobalVariable.rawgUrl}${GlobalVariable.gamesPath}/${gameId}${GlobalVariable.seriesPath}?key=${GlobalVariable.key}</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>'3498'</defaultValue>
+      <description></description>
+      <id>fd0fb99a-fd3f-4b55-9f2f-649643acb7ec</id>
+      <masked>false</masked>
+      <name>gameId</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
